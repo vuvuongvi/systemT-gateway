6e30ee5ad5692f8b17ff7a841b72894e02c92848
root <root@localhost.localdomain>
2020-02-07 08:46:04 -0500

    Update code federation for microservices

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -1,6 +1,26 @@
-const path = require('path');
-const gateway = require('express-gateway');
+const { ApolloServer } = require("apollo-server");
+const { ApolloGateway } = require("@apollo/gateway");
+const gateway = new ApolloGateway({
+    serviceList: [
+        { name: 'userApi', url: 'http://localhost:3002/graphql' },
+    ],
+    __exposeQueryPlanExperimental: false,
+});
 
-gateway()
-  .load(path.join(__dirname, 'config'))
-  .run();
+(async () => {
+    const server = new ApolloServer({
+        gateway,
+
+        // Apollo Graph Manager (previously known as Apollo Engine)
+        // When enabled and an `ENGINE_API_KEY` is set in the environment,
+        // provides metrics, schema management and trace reporting.
+        engine: false,
+
+        // Subscriptions are unsupported but planned for a future Gateway version.
+        subscriptions: false,
+    });
+
+    server.listen().then(({ url }) => {
+        console.log(`🚀 Server ready at ${url}`);
+    });
+})();
